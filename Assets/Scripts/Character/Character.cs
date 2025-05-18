3b09eed1b59620a4a89cb79b5f7793515bf50100
anaclars2 <166453698+anaclars2@users.noreply.github.com>
2025-05-18 17:38:37 -0300

    Animation

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -1,35 +1,80 @@
+using System.Collections;
 using UnityEditor.Animations;
 using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
 public class Character : MonoBehaviour
 {
+    [Header("Move")]
     [SerializeField] float speed = 6.0f;
     CharacterController characterController;
-    [SerializeField] Animator animatorController;
-    [SerializeField] KeyCode InputRun;
-    [SerializeField] KeyCode InputPunch;
-    float value = 0;
+    float moveValue = 0f;
 
-    void Start() { characterController = GetComponent<CharacterController>(); }
+    [Header("Animation")]
+    [SerializeField] Animator animatorController;
+    KeyCode inputRunning = KeyCode.LeftShift, inputBoxing = KeyCode.E;
+    float boxingWeight = 0f;
+    int armsLayerIndex;
+
+    bool isBoxing = false;
+
+    void Start()
+    {
+        armsLayerIndex = animatorController.GetLayerIndex("Arms");
+        characterController = GetComponent<CharacterController>();
+    }
 
     void Update()
     {
+        HandleMovement();
+        HandleBoxing();
+    }
+
+    void HandleMovement()
+    {
         float x = Input.GetAxis("Horizontal");
+
         if (Input.GetButton("Horizontal"))
         {
             Vector3 move = transform.right * x + transform.forward;
             characterController.Move(move * speed * Time.deltaTime);
 
-            // animation
-            if (Input.GetKey(InputRun)) { value += 0.05f; }
-            else if (x >= 0.5f) { value = 0.25f; }
-            animatorController.SetFloat("Move", value);
+            // controlando valor de animacao da corrida
+            if (Input.GetKey(inputRunning)) { moveValue = Mathf.MoveTowards(moveValue, 1f, Time.deltaTime); }
+            else { moveValue = Mathf.MoveTowards(moveValue, 0.25f, Time.deltaTime); }
+
+            animatorController.SetFloat("Move", moveValue);
         }
         else
         {
-            animatorController.SetFloat("Move", 0f);
-            Debug.Log(0);
+            moveValue = Mathf.MoveTowards(moveValue, 0f, Time.deltaTime * 5f);
+            animatorController.SetFloat("Move", moveValue);
         }
+    }
+
+    void HandleBoxing()
+    {
+        if (Input.GetKeyDown(inputBoxing) && !isBoxing)
+        {
+            isBoxing = true;
+            animatorController.Play("Boxing", armsLayerIndex, 0f);
+            StartCoroutine(BoxingLayerWeightRoutine());
+        }
+
+        // transicao suave visualmente entre layers
+        float targetWeight = isBoxing ? 1f : 0f;
+        boxingWeight = Mathf.Lerp(boxingWeight, targetWeight, Time.deltaTime * 5f);
+        animatorController.SetLayerWeight(armsLayerIndex, boxingWeight);
+    }
+
+    IEnumerator BoxingLayerWeightRoutine()
+    {
+        // esperando o tempo da animacao
+        AnimatorClipInfo[] clips = animatorController.GetCurrentAnimatorClipInfo(armsLayerIndex);
+        if (clips.Length > 0) { yield return new WaitForSeconds(clips[0].clip.length); }
+        isBoxing = false;
+
+        int currentAnimationBaseLayer = animatorController.GetCurrentAnimatorClipInfoCount(0);
+        animatorController.CrossFade(currentAnimationBaseLayer, 0.1f, 0);
     }
 }
